3f5bf607c4f70128ed64485bab01a4f130f9706e
Mahtab Mustakim <mtbbsl@gmail.com>
2023-05-18 03:04:07 +0600

    footer added

---
+++ src/pages/Shared/Footer/Footer.jsx
@@ -0,0 +1,65 @@
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "@fortawesome/fontawesome-free/css/fontawesome.min.css";
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-900 text-white">
+      <div className="container mx-auto py-8 px-4">
+        <div className="flex flex-wrap">
+          <div className="w-full md:w-1/4 mb-6 md:mb-0">
+            <img className="w-64 mb-4" src="/logo-w.png" alt="Logo" />
+            <p>&copy; 2023 AlimamaCarZone. All rights reserved.</p>
+          </div>
+          <div className="w-full md:w-1/4 mb-6 md:mb-0">
+            <h3 className="text-lg font-semibold mb-4">Pages</h3>
+            <ul className="list-none">
+              <li className="mb-2">
+                <a href="#">Home</a>
+              </li>
+              <li className="mb-2">
+                <a href="#">Products</a>
+              </li>
+              <li className="mb-2">
+                <a href="#">About</a>
+              </li>
+              <li className="mb-2">
+                <a href="#">Contact</a>
+              </li>
+            </ul>
+          </div>
+          <div className="w-full md:w-1/4 mb-6 md:mb-0">
+            <h3 className="text-lg font-semibold mb-4">Follow Us</h3>
+            <div className="flex space-x-4">
+              <a
+                href="#"
+                className="text-white hover:text-gray-400 transition-colors duration-300"
+              >
+                <i className="fab fa-facebook-f"></i>
+              </a>
+              <a
+                href="#"
+                className="text-white hover:text-gray-400 transition-colors duration-300"
+              >
+                <i className="fab fa-twitter"></i>
+              </a>
+              <a
+                href="#"
+                className="text-white hover:text-gray-400 transition-colors duration-300"
+              >
+                <i className="fab fa-instagram"></i>
+              </a>
+            </div>
+          </div>
+          <div className="w-full md:w-1/4 mb-6 md:mb-0">
+            <h3 className="text-lg font-semibold mb-4">Contact</h3>
+            <p>123 Street, City, Country</p>
+            <p>Email: example@example.com</p>
+            <p>Phone: +123 456 789</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
