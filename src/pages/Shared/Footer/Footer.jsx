7427f3d54791cd2d238031ed41bdfce6cb7a5558
Mahtab Mustakim <mtbbsl@gmail.com>
2023-05-18 18:24:25 +0600

    modified header and footer

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -4,60 +4,44 @@ import logo from '/logo-w.png';
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-900 text-white">
-      <div className="container mx-auto py-8 px-4">
-        <div className="flex flex-wrap">
-          <div className="w-full md:w-1/4 mb-6 md:mb-0">
-            <img className="h-12 mb-4" src={logo} alt="Logo" />
-            <p>&copy; 2023 AlimamaCarZone. All rights reserved.</p>
-          </div>
-          <div className="w-full md:w-1/4 mb-6 md:mb-0">
-            <h3 className="text-lg font-semibold mb-4">Pages</h3>
-            <ul className="list-none">
-              <li className="mb-2">
-                <a href="#">Home</a>
-              </li>
-              <li className="mb-2">
-                <a href="#">Products</a>
-              </li>
-              <li className="mb-2">
-                <a href="#">About</a>
-              </li>
-              <li className="mb-2">
-                <a href="#">Contact</a>
-              </li>
-            </ul>
-          </div>
-          <div className="w-full md:w-1/4 mb-6 md:mb-0">
-            <h3 className="text-lg font-semibold mb-4">Follow Us</h3>
-            <div className="flex space-x-4">
-              <a
-                href="#"
-                className="text-white hover:text-gray-400 transition-colors duration-300"
-              >
-                <i className="fab fa-facebook-f"></i>
-              </a>
-              <a
-                href="#"
-                className="text-white hover:text-gray-400 transition-colors duration-300"
-              >
-                <i className="fab fa-twitter"></i>
-              </a>
-              <a
-                href="#"
-                className="text-white hover:text-gray-400 transition-colors duration-300"
-              >
-                <i className="fab fa-instagram"></i>
-              </a>
-            </div>
-          </div>
-          <div className="w-full md:w-1/4 mb-6 md:mb-0">
-            <h3 className="text-lg font-semibold mb-4">Contact</h3>
-            <p>123 Street, City, Country</p>
-            <p>Email: example@example.com</p>
-            <p>Phone: +123 456 789</p>
+    <footer className="footer p-10 bg-gray-900 text-white">
+      <div>
+        <img src={logo} alt="Logo" className="h-12 mb-4" />
+        <p>&copy; 2023 AlimamaCarZone. All rights reserved.</p>
+        <div className="flex flex-row items-center">
+          <h3 className="text-lg font-semibold mr-4">Follow Us</h3>
+          <div className="flex space-x-4">
+            <a href="#" className="text-white hover:text-yellow-400 transition-colors duration-300">
+              <i className="fab fa-facebook-f"></i>
+            </a>
+            <a href="#" className="text-white hover:text-yellow-400 transition-colors duration-300">
+              <i className="fab fa-twitter"></i>
+            </a>
+            <a href="#" className="text-white hover:text-yellow-400 transition-colors duration-300">
+              <i className="fab fa-instagram"></i>
+            </a>
           </div>
         </div>
+      </div>
+      <div>
+        <span className="footer-title">Services</span>
+        <a className="link link-hover">Branding</a>
+        <a className="link link-hover">Design</a>
+        <a className="link link-hover">Marketing</a>
+        <a className="link link-hover">Advertisement</a>
+      </div>
+      <div>
+        <span className="footer-title">Company</span>
+        <a className="link link-hover">About us</a>
+        <a className="link link-hover">Contact</a>
+        <a className="link link-hover">Jobs</a>
+        <a className="link link-hover">Press kit</a>
+      </div>
+      <div>
+        <span className="footer-title">Legal</span>
+        <a className="link link-hover">Terms of use</a>
+        <a className="link link-hover">Privacy policy</a>
+        <a className="link link-hover">Cookie policy</a>
       </div>
     </footer>
   );
--- src/pages/Shared/NavBar/NavBar.jsx
+++ src/pages/Shared/NavBar/NavBar.jsx
@@ -1,24 +1,101 @@
 import { Link } from 'react-router-dom';
 import logo from '/logo-w.png';
-import avater from '/photo.jpg';
+import avatar from '/photo.jpg';
 
 const NavBar = () => {
 
   return (
-    <div className="navbar bg-gray-900 text-white px-6 py-6 flex flex-col lg:flex-row gap-6">
-      <div className="flex-1">
-        <a className='btn btn-ghost'>
-          <img className="h-16" src={logo} alt="logo" />
-        </a>
-      </div>
-      <div className='flex-none gap-0'>
-        <Link to='/' className='btn btn-ghost normal-case text-sm'>Home</Link>
-        <Link className='btn btn-ghost normal-case text-sm'>Blog</Link>
-        <div className='flex items-center'>
-          <img src={avater} alt="profile picture" className='w-12 rounded-full cursor-pointer'/>
-          <button className='btn btn-ghost normal-case text-sm'>Sign Out</button>
+    <div className="navbar bg-gray-900 text-white">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <a>Item 1</a>
+            </li>
+            <li tabIndex={0}>
+              <a className="justify-between">
+                Parent
+                <svg
+                  className="fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
+                </svg>
+              </a>
+              <ul className="p-2">
+                <li>
+                  <a>Submenu 1</a>
+                </li>
+                <li>
+                  <a>Submenu 2</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a>Item 3</a>
+            </li>
+          </ul>
         </div>
-        <Link className='btn btn-ghost normal-case text-sm'>Login</Link>
+        <Link to="/" className="btn btn-ghost normal-case text-xl">
+          <img src={logo} alt="logo" className='h-16' />
+        </Link>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <a>Item 1</a>
+          </li>
+          <li tabIndex={0}>
+            <a>
+              Parent
+              <svg
+                className="fill-current"
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+              >
+                <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
+              </svg>
+            </a>
+            <ul className="p-2">
+              <li>
+                <a>Submenu 1</a>
+              </li>
+              <li>
+                <a>Submenu 2</a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <a>Item 3</a>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <a className="btn">Get started</a>
       </div>
     </div>
   );
