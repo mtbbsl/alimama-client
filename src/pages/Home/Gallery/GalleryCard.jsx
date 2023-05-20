c777f33e94923d82eb1d33a0690f5fead7a58a53
Mahtab Mustakim <mtbbsl@gmail.com>
2023-05-21 03:07:12 +0600

    modified gallery section

--- src/pages/Home/Gallery/GalleryCard.jsx
+++ src/pages/Home/Gallery/GalleryCard.jsx
@@ -1,23 +1,11 @@
 import { Link } from "react-router-dom";
 
 const GalleryCard = ({ gallery }) => {
-  const { title, img, price, rating } = gallery;
+  const { img } = gallery;
 
   return (
     <div className="card w-96 bg-base-100 shadow-xl">
-      <figure>
-        <img src={img} alt="" className="object-cover h-64 w-96" />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title">{title}</h2>
-        <p>Price: ${price}</p>
-        <p>Rating: {rating}</p>
-        <div className="card-actions justify-end">
-          <Link to="/details">
-            <button className="btn btn-outline btn-warning">View Details</button>
-          </Link>
-        </div>
-      </div>
+      <img src={img} alt="" className="object-cover h-64 w-96 rounded-xl p-4" />
     </div>
   );
 };
